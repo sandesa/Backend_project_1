598c12ca56dd17d016cb48f198a26a02b017c60f
Sampe <samuel.sandenas123@gmail.com>
2024-04-14 22:11:29 +0200

    Added: - Models: Product.cs - Data: ProductsSeedData.cs, AppDbContext
    Updated:
    - Data: AppDbContext, lade till kopplingen till databasen.
    - Program.cs: Initiated the data seeding.

---
+++ Webbshop/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webbshop.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public string Category { get; set; }
+		[Required]
+		public string Description { get; set; }
+		[Required]
+		public decimal Price { get; set; }
+		[Required]
+		public string ImageName { get; set; }
+
+	}
+}
