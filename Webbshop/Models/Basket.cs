26c2bcb4424946c47286126edf3d3d98f956b172
Sampe <samuel.sandenas123@gmail.com>
2024-04-15 22:11:40 +0200

    Test

---
+++ Webbshop/Models/Basket.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Webbshop.Models
+{
+    public class Basket
+    {
+        public List<OrderItem> Items { get; set; }
+        public int NumberOfItems => Items.Sum(p => p.Quantity);
+    }
+}
---
+++ Webbshop/Models/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Webbshop.Models
+{
+    public class OrderItem
+    {
+        public int ProductId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
